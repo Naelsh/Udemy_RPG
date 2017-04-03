9f52aab7c40f596b8979c92a68a4b3ea43dd4cd5
Niklas Lindblad <niklas.o.lindblad@gmail.com>
2017-04-03 06:44:46 +0200

    14 Using Cursor Affordances

---
+++ RPG Course/Assets/Cameras & UI/CursorAffordance.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorAffordance : MonoBehaviour {
+    
+    [SerializeField]
+    Texture2D walkCursor = null;
+    [SerializeField]
+    Texture2D attackCursor = null;
+    [SerializeField]
+    Texture2D questionCursor = null;
+
+    private CameraRaycaster cameraRaycaster;
+    private Vector2 cursorHotSpot;
+
+    // Use this for initialization
+    void Start () {
+        cameraRaycaster = FindObjectOfType<CameraRaycaster>();
+        cursorHotSpot = new Vector2(96,96);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        switch (cameraRaycaster.layerHit)
+        {
+            case Layer.Default:
+                break;
+            case Layer.TransparentFX:
+                break;
+            case Layer.IgnoreRaycast:
+                break;
+            case Layer.Water:
+                break;
+            case Layer.UI:
+                break;
+            case Layer.Walkable:
+                Cursor.SetCursor(walkCursor, cursorHotSpot, CursorMode.Auto);
+                break;
+            case Layer.Enemy:
+                Cursor.SetCursor(attackCursor, cursorHotSpot, CursorMode.Auto);
+                break;
+            case Layer.RaycastEndStop:
+                Cursor.SetCursor(questionCursor, cursorHotSpot, CursorMode.Auto);
+                break;
+            default:
+                print("Don't know what cursor to show");
+                break;
+        }
+        
+	}
+}
